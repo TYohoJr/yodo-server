b93bc6563fe624cdced4226b1f77a1a2bf79ae6c
Thomas Yoho <TYohoJr@Gmail.com>
2019-04-10 14:09:30 -0500

    added template for sign up, log in, change password

--- express.js
+++ express.js
@@ -29,3 +29,124 @@ client.connect((err) => {
         })
     }
 })
+
+function verifyToken(req, res, next) {
+    var token = req.body.token;
+    if (token) {
+        jwt.verify(token, "Secret", (err, decode) => {
+            if (err) {
+                res.send("Wrong token")
+            } else {
+                res.locals.decode = decode
+                next();
+            }
+        })
+    } else {
+        console.log("no token")
+        res.send("No token")
+    }
+}
+
+app.get('/', function (req, res) {
+    res.sendFile(path.join(__dirname, 'build', 'index.html'));
+});
+
+// app.post('/signUpData', (req, res) => {
+//     if (req.body.username.length && req.body.password.length) {
+//         db.collection('users').find({ username: req.body.username }).toArray((err, user) => {
+//             if (user.length) {
+//                 res.json('This username already exists')
+//             } else {
+//                 bcrypt.hash(req.body.password, saltRounds, function (err, hash) {
+//                     db.collection('users').save({
+//                         username: req.body.username,
+//                         password: hash,
+//                         staticDots: [],
+//                         pursuits: [],
+//                         saccades: [],
+//                         combination: [],
+//                         randomSaccades: [],
+//                         antiSaccades: [],
+//                         opk: [],
+//                         dateCreated: req.body.dateCreated
+//                     }, (err, result) => {
+//                         if (err) {
+//                             res.json("Failed")
+//                             return console.log(err);
+//                         } else {
+//                             res.json("Sign Up Successful")
+//                             console.log('saved to database');
+//                         }
+//                     });
+//                 });
+//             }
+//         })
+//     } else {
+//         res.json('Error: username or password can\'t be blank')
+//     }
+// });
+
+// app.post('/changePassword', (req, res) => {
+//     db.collection('users').find({ username: req.body.username }).toArray((err, user) => {
+//         if (user.length) {
+//             bcrypt.compare(req.body.oldPassword, user[0].password, function (err, resolve) {
+//                 if (resolve) {
+//                     bcrypt.hash(req.body.newPassword1, saltRounds, function (err, hash) {
+//                         db.collection('users').updateOne(
+//                             { username: req.body.username },
+//                             {
+//                                 $set:
+//                                 {
+//                                     password: hash
+//                                 }
+//                             }
+//                         )
+//                         if (err) {
+//                             res.json("Failed")
+//                             console.log(err);
+//                         } else {
+//                             res.json('Successfully updated password')
+//                             console.log(`password updated for ${req.body.username}`);
+//                         }
+//                     });
+//                 } else {
+//                     res.json("Wrong password")
+//                 }
+//             });
+//         } else {
+//             res.json('Error: Please log out and back in')
+//         }
+//     })
+// });
+
+// app.post("/userLogIn", (req, res) => {
+//     db.collection("users").find({ username: req.body.username }).toArray((err, user) => {
+//         if (!user.length) {
+//             res.json({
+//                 message: `Login failed!`
+//             });
+//         } else if (err) {
+//             res.json({
+//                 message: "Login failed!"
+//             });
+//         } else {
+//             bcrypt.compare(req.body.password, user[0].password, function (err, resolve) {
+//                 if (resolve === true) {
+//                     var token = jwt.sign(req.body.username, ('Secret'), {
+//                     });
+//                     res.json({
+//                         message: "Login successful!",
+//                         myToken: token,
+//                         user: user[0],
+//                         item: user
+//                     });
+//                     console.log(`Sign in successful from ${req.body.username}`)
+//                 } else if (resolve === false) {
+//                     res.json({
+//                         message: "Login failed!",
+//                     })
+//                 }
+//             });
+//         }
+//     })
+// });
